74d2031999e6c2d25270c78cbdc8415059af6821
Anjali Rawat <62091771+dev-bugger@users.noreply.github.com>
2024-07-10 17:08:46 +0530

    adding spinner

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -2,16 +2,20 @@ import React, { memo } from "react";
 
 import Header from "components/Header";
 import Socials from "components/Socials";
+import Loader from "components/Loader";
 
 const View: React.FC<ViewProps> = ({ children }) => {
   return (
-    <div className="d-flex flex-column h-100 w-100 view">
-      <Header />
-      <section className="overflow-hidden d-flex flex-grow-1">
-        <Socials />
-        <main className="flex-grow-1 app-body">{children}</main>
-      </section>
-    </div>
+    <>
+      <Loader />
+      <div className="d-flex flex-column h-100 w-100 view">
+        <Header />
+        <section className="overflow-hidden d-flex flex-grow-1">
+          <Socials />
+          <main className="flex-grow-1 app-body">{children}</main>
+        </section>
+      </div>
+    </>
   );
 };
 
